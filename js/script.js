adf6c42986cebcfdc214f5d1a0356bb321537d1c
mswarlord <canterofacundo@hotmail.com>
2022-07-16 03:29:38 -0300

    Mejora de codigo
    Se realizó refactorización del codigo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
-let ficha = []; //array que guarda los nodos ficha
-let mapaActual = 1; //Indica cual es el mapa seleccionado
-const fichas = []; //guadrda el array de objetos ficha
-const mapas = []; //genera el array mapas
-let fichaSeleccionada; // indica la ficha seleccionada
-let fichaColocada; //indica la ficha colocada actualmente
+//*************************************************************************************************************************
+//************************************************************ VARIABLES **************************************************
+//*************************************************************************************************************************
+let ficha = []; //array que guarda los nodos ficha.
+let mapaActual = 1; //Indica cual es el mapa seleccionado.
+const fichas = []; //guadrda el array de objetos ficha.
+const mapas = []; //genera el array mapas.
+let fichaSeleccionada; // indica la ficha seleccionada.
+let fichaColocada; //indica la ficha colocada actualmente.
+let slots = []; //array con los slots del DOM y sus estados.
 
-/* const slots = [ //pasar este array luego a JSON
-    document.getElementById("slot1"),
-    document.getElementById("slot2"),
-    document.getElementById("slot3"),
-    document.getElementById("slot4"),
-    document.getElementById("slot5"),
-    document.getElementById("slot6"),
-    document.getElementById("slot7"),
-    document.getElementById("slot8"),
-    document.getElementById("slot9")
-]
- */
-let slots = [];
+
+//*************************************************************************************************************************
+//*************************************************************** CLASES **************************************************
+//*************************************************************************************************************************
+
+//CLASE SLOT ***************************************************************************
 class Slot{//Se crea la clase Slot que establece el elemento del DOM y su estado.
     constructor(elementoDOM) {
         this.elementoDOM = elementoDOM;
         this.ocupado = false;//inicia el slot como desocupado.
+        this.oculto = false;
+    }
+    
+    ocultarSlot() {
+            this.elementoDOM.style.display = "none";
     }
 
+    mostrarSlot() {
+            this.elementoDOM.style.display = "";
+    }
+    
     ocuparSlot() {
         this.ocupado = true;
     }
@@ -33,19 +39,10 @@ class Slot{//Se crea la clase Slot que establece el elemento del DOM y su estado
     }
 }
 
-
-
-window.onload = function() {
-    //*********************************************************************************************************************************
-    for(i=0;i<9;i++){ //iteración para la creación de las fichas
-    
-        slots[i]=new Slot(document.getElementById(`slot${+i+1}`));
-    }
-//************************************************* CREACION DE LAS FICHAS Y SUS ESTADOS ******************************************
-//*********************************************************************************************************************************
+//CLASE FICHA **************************************************************************
 class Ficha{//Se crea la clase Ficha donde se establece la posicion inicial y los metodos para cambiar la posicion de las mismas
     constructor(id) {
-        this.id = id    ;
+        this.id = id;
         this.posicion = 1;//inicia las fichas en una posicion determinada
         this.seleccionada = false; //indica si la ficha esta seleccionada
     }
@@ -55,11 +52,13 @@ class Ficha{//Se crea la clase Ficha donde se establece la posicion inicial y lo
             this.seleccionada = true;
             console.log(`seleccionaste la ficha ${this.id}`);
             fichaSeleccionada = this.id;
+
             for(let e of fichas){ //Esta iteración deselecciona cualquier ficha que este seleccionada 
                 if(e !== this){   //restringiendo al usuario que pueda seleccionar mas de una ficha
                     e.seleccionada = false;
                 }
             }
+
         }else{
             this.seleccionada = false;
             console.log(`Deseleccionaste la ficha ${this.id}`)
@@ -86,14 +85,7 @@ class Ficha{//Se crea la clase Ficha donde se establece la posicion inicial y lo
     }
 }
 
-for(i=0;i<7;i++){ //iteración para la creación de las fichas
-    fichas[i]=new Ficha(i);
-}
-
-//*********************************************************************************************************************************
-//************************************************* CREACION DE MAPAS Y SUS ESTADOS ***********************************************
-//*********************************************************************************************************************************
-
+//CLASE MAPA ***************************************************************************
 class mapa { //genera la clase constructora de objetos mapa
     constructor(nivel, dificultad, img){
         this.nivel = nivel + 1;
@@ -122,28 +114,52 @@ class mapa { //genera la clase constructora de objetos mapa
     }
 }
 
-const {mapaHabilitado, mapaCompleto, slotsDelMapa} = mapas; //desestructura los atributos mas usado en mapas
+//*************************************************************************************************************************
+//********************************************************FUNCIONES********************************************************
+//*************************************************************************************************************************
 
-for(i=0;i<50;i++){
-    mapas[i]=new mapa(i,i,`assets/img/nivel${i+1}.jpg`);
-    if(mapas[i].nivel < 3) {
-        mapas[i].slotsDelMapa = 1;
-    }else if(mapas[i].nivel < 11) {
-        mapas[i].slotsDelMapa = 2;
-    }else if(mapas[i].nivel < 21){
-        mapas[i].slotsDelMapa = 3;
-    }else if(mapas[i].nivel < 31){
-        mapas[i].slotsDelMapa = 4;
-    }else if(mapas[i].nivel < 41){
-        mapas[i].slotsDelMapa = 5;
-    }else{
-        mapas[i].slotsDelMapa = 6;
+let mostrarSlots = () => { //funcion que oculta los Slots de la zona de juego
+    for(let e in slots){
+        slots[e].mostrarSlot();
+    }
+}
+
+let ocultarSlots = () => { //funcion que oculta los Slots de la zona de juego
+    for(let e in slots){
+        slots[e].ocultarSlot();
     }
 }
 
 //*********************************************************************************************************************************
+//************************************************* CREACION DE LAS FICHAS Y SUS ESTADOS ******************************************
 //*********************************************************************************************************************************
+
+for(i=0;i<7;i++){ //iteración para la creación de las fichas
+    fichas[i]=new Ficha(i);
+}
+
 //*********************************************************************************************************************************
+//************************************************* CREACION DE MAPAS Y SUS ESTADOS ***********************************************
+//*********************************************************************************************************************************
+
+const {mapaHabilitado, mapaCompleto, slotsDelMapa} = mapas; //desestructura los atributos mas usado en mapas
+
+for(i=0;i<50;i++){ //itera 50 veces
+    mapas[i]=new mapa(i,i,`assets/img/nivel${i+1}.jpg`); //cada iteración crea un objeto mapa dentro del array mapas
+    if(mapas[i].nivel < 3) { 
+        mapas[i].slotsDelMapa = 1; //si el nivel del mapa es < a 3 le coloca 1 slot
+    }else if(mapas[i].nivel < 11) {
+        mapas[i].slotsDelMapa = 2; //si el nivel del mapa es < a 11 le coloca 2 slots
+    }else if(mapas[i].nivel < 21){
+        mapas[i].slotsDelMapa = 3; //si el nivel del mapa es < a 21 le coloca 3 slots
+    }else if(mapas[i].nivel < 31){
+        mapas[i].slotsDelMapa = 4; //si el nivel del mapa es < a 31 le coloca 4 slots
+    }else if(mapas[i].nivel < 41){
+        mapas[i].slotsDelMapa = 5; //si el nivel del mapa es < a 41 le coloca 5 slots
+    }else{
+        mapas[i].slotsDelMapa = 6; //si el nivel del mapa es >= a 41 le coloca 6 slots
+    }
+}
 
 //*********************************************************************************************************************************
 //**************************************** CREACION DE SLOTS PARA LOS MAPAS *******************************************************
@@ -154,12 +170,15 @@ for(i=0;i<50;i++){
     La cantidad de slots dependerá del atributo slotsDelMapa del objeto mapas. 
     Luego hay que determinar que ficha va en cada slot y en que posición para que el mapa esté completo...
 */
+for(i=0;i<9;i++){ //iteración para la creación de los slots
+    slots[i]=new Slot(document.getElementById(`slot${+i+1}`));
+}
 
 for(e in slots){ //quita del DOM todos los slots
     slots[e].elementoDOM.style.display = "none";
 } 
 
-slots[0].elementoDOM.style.display = ""; //muestra en el DOM el Slot con indice 0 del array
+
 
 /* 
     EN ESTE PUNTO TENGO QUE TOMAR EL VALOR DE LA POSICION QUE TOMA LA FICHA AL MOMENTO QUE ES GIRADA
@@ -168,8 +187,10 @@ slots[0].elementoDOM.style.display = ""; //muestra en el DOM el Slot con indice 
 */
 
 const clickSlots = () => {
-    if(!!fichaColocada){
-
+    if(!!fichaSeleccionada){
+        colocarFicha();
+    }else if(!!slots[e].ocupado){ 
+        rotarFicha();
     }
 }
 
@@ -248,86 +269,63 @@ let colocarFicha = (e,n) => {
     }
 }
 
-
-/*
-for(let e of slot){
-    slot1.addEventListener('click',function() {colocarFicha(slot1)});
-}; */
-
-slot1.addEventListener('click',function() {colocarFicha(slot1,0)});
-slot2.addEventListener('click',function() {colocarFicha(slot2,1)});
-slot3.addEventListener('click',function() {colocarFicha(slot3,2)});
-slot4.addEventListener('click',function() {colocarFicha(slot4,3)});
-slot5.addEventListener('click',function() {colocarFicha(slot5,4)});
-slot6.addEventListener('click',function() {colocarFicha(slot6,5)});
-slot7.addEventListener('click',function() {colocarFicha(slot7,6)});
-slot8.addEventListener('click',function() {colocarFicha(slot8,7)});
-slot9.addEventListener('click',function() {colocarFicha(slot9,8)});
-
-//CODIGO PARA LA CREACION DE SLOTS MODIFICANDO EL DOM POR JS
-/* 
-let divSlot = document.getElementById("slots");//Obtiene el nodo donde se van a agregar los nuevos elementos - en este caso en un <DIV>
-
-let generarSlots = () => { //funcion que genera los slots de cada mapa
-    for( i=0; i < mapas[mapaActual].slotsDelMapa;i++) { //itera tantas veces como el valor slotsDelMapa tenga mapaActual
-        let div = document.createElement("div");//Crea un nodo <div> en cada iteración. 
-        div.id = `slot${i+1}`;
-        div.className = `slots`;
-        div.innerHTML = ``;
-        divSlot.appendChild(div);
-    }
-}    
-
-let resetearSlots = () => { //funcion que elimina los Slots creados en el mapa anterior
-divSlot.remove();
-divSlot = document.getElementById("slots");
-}  */
-
-//*********************************************************************************************************************************
-//*********************************************************************************************************************************
-//*********************************************************************************************************************************
+for(e in slots){              //itero los elementos del array slots
+    let i = e;                //crea una variable para meter en slots[i] -- creo la variable i para solucionar un error
+    slots[e].elementoDOM.addEventListener('click',function() {colocarFicha(slots[i].elementoDOM,e)}); //genera en cada
+    //iteración un evento click en cada slot, con la función colocar ficha y el slot que se está clickeando como parámetro.
+    //ademas meto otro parámetro que por ahora no estoy usando. borrar antes de la entrega
+} 
 
 //*********************************************************************************************************************************
 //**************************************************** CAMBIAR MAPA ***************************************************************
 //*********************************************************************************************************************************
+//DOM
 let mapaSeleccionado = document.getElementById("mapaSeleccionado");
 let btnSiguiente = document.getElementById('btnSiguiente');
+let btnAnterior = document.getElementById("btnAnterior");
+//
 mapas[0].seleccionado = true;
+slots[0].elementoDOM.style.display = ""; //muestra en el DOM el Slot con indice 0 del array
 
 //MAPA SIGUIENTE
 let mapaSiguiente = () => {
+    if(mapas[mapaActual].mapaCompleto){
     mapaActual++;
     mapaSeleccionado.src=`../assets/img/niveles/nivel${mapaActual}.png`;
     mapas[mapaActual-1].seleccionado = true; //indica al mapa actual como seleccionado.
     mapas[mapaActual-2].seleccionado = false; //indica al mapa anterior como no seleccionado.
-    resetearSlots();
-    generarSlots(); //VER SECCION GENERAR SLOTS
+    ocultarSlots(); //oculta todos los slots
+    //mostrarSlots(); //muestra los slots necesarios para el mapa siguiente
+    }else{
+        alert("Tienes que completar este nivel para pasar al siguiente");
+    }
 }
 
-btnSiguiente.addEventListener('click',mapaSiguiente);
+btnSiguiente.addEventListener('click',mapaSiguiente); //boton que pasa al mapa siguiente
 
 //MAPA ANTERIOR
-let btnAnterior = document.getElementById("btnAnterior");
+
 let mapaAnterior = () => {
     mapaActual--;
     mapaSeleccionado.src=`../assets/img/niveles/nivel${mapaActual}.png`;
     mapas[mapaActual-1].seleccionado = true; //indica al mapa actual como seleccionado.
     mapas[mapaActual].seleccionado = false; //indica al mapa anterior como no seleccionado.
 }
-btnAnterior.addEventListener('click',mapaAnterior);
+btnAnterior.addEventListener('click',mapaAnterior); //boton que pasa al mapa anterior
 
 //*********************************************************************************************************************************
 //*********************** CREACION DE LOS CONTENEDORES CON LAS FICHAS E ITERACION DE ARRAY ****************************************
 //*********************************************************************************************************************************
 
-let divFicha = document.getElementById("divFichas");//Obtiene el nodo donde se van a agregar los nuevos elementos - en este caso en un <DIV>
-
-for (const divFichaN of fichas) {//Itera el array fichas, con for...of.
+//DOM
+let divFicha = document.getElementById("divFichas");//Obtiene el nodo <DIV> donde se van a agregar los nuevos elementos - 
+//
+for (let divFichaN of fichas) {//Itera el array fichas, con for...of.
     let div = document.createElement("div");//Crea un nodo <div> en cada iteración. 
     div.id = `ficha${divFichaN.id + 1}`;//le asigno al nuevo div el ID ficha"N", donde "N" es el numero de ficha.
     div.className = `ficha`; //le asigno a cada DIV la clase ficha.
     div.innerHTML = `<img src="../assets/img/fichas/ficha${divFichaN.id +1}.png" alt="">` //asigna a cada nodo la etiqueta IMG con el valor correspondiente al elemento de la iteración actual del array fichas
-    divFicha.appendChild(div); //Se inserta el nuevo nodo al padre en cada ciclo
+    divFicha.appendChild(div); //Se inserta el nuevo nodo DIV al padre en cada ciclo
 }
 
 //*********************************************************************************************************************************
@@ -358,7 +356,6 @@ MOSTRARFICHAS();
 let seleccionarFicha = e => {
     fichas[e].fichaSeleccionada();
 } 
-
 
 for(let e in ficha){
 ficha[e].addEventListener('click',function() {seleccionarFicha(e)});
@@ -406,8 +403,3 @@ for(const nroFicha of fichas) {//Recorre los elementos del array ficha.
 //*********************************************************************************************************************************
 //*********************************************************************************************************************************
 //*********************************************************************************************************************************
-
-
-
-
-}
